b8c2f1488a7b0a36814612b7acdb66323609591f
MN <minhfnhat@gmail.com>
2022-06-27 13:27:50 +0700

    ReadyToUniitTest

--- src/test/java/com/example/SellingBreadApp/controller/IngredientControllerTest.java
+++ src/test/java/com/example/SellingBreadApp/controller/IngredientControllerTest.java
@@ -1,9 +1,16 @@
 package com.example.SellingBreadApp.controller;
+import com.example.SellingBreadApp.repository.ToppingRepository;
+import com.example.SellingBreadApp.service.ToppingService;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.web.servlet.MockMvc;
 
 
 @SpringBootTest
 class IngredientControllerTest {
+  private ToppingService toppingService;
+  private ToppingRepository toppingRepository;
+
+  private MockMvc mockMvc;
+
 
 }
