81661789196521fd0d574f092b6afb4027f2e400
MN <minhfnhat@gmail.com>
2022-06-27 11:41:13 +0700

    changeToMakeTest

--- src/test/java/com/example/SellingBreadApp/controller/IngredientControllerTest.java
+++ src/test/java/com/example/SellingBreadApp/controller/IngredientControllerTest.java
@@ -1,14 +1,6 @@
 package com.example.SellingBreadApp.controller;
-import com.example.SellingBreadApp.service.ProductService;
-import com.example.SellingBreadApp.service.ToppingService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 
 
 @SpringBootTest
