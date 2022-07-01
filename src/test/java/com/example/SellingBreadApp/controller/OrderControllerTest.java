6244b77cfc4a0b82f3a5b331b753ad94d303f0fc
MN <minhfnhat@gmail.com>
2022-07-01 15:53:56 +0700

    finalMerge

--- src/test/java/com/example/SellingBreadApp/controller/OrderControllerTest.java
+++ src/test/java/com/example/SellingBreadApp/controller/OrderControllerTest.java
@@ -29,7 +29,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-
+class OrderControllerTest{
+  private MockMvc mockMvc;
   @Autowired
   private ExceptionControllerAdvice exceptionControllerAdvice;
 
